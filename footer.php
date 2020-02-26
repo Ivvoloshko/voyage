fa4056d7bcd14596483b8b01fc8da4e8586529a4
IV <iv.voloshko@gmail.com>
2020-02-26 19:47:59 +0200

    new file:   .gitignore 	new file:   404.php 	new file:   LICENSE 	new file:   README.md 	new file:   archive.php 	new file:   assets/css/bootstrap.min.css 	new file:   assets/css/main.css 	new file:   assets/js/bootstrap.min.js 	new file:   assets/js/main.js 	new file:   comments.php 	new file:   footer.php 	new file:   functions.php 	new file:   header.php 	new file:   inc/custom-header.php 	new file:   inc/customizer.php 	new file:   inc/jetpack.php 	new file:   inc/template-functions.php 	new file:   inc/template-tags.php 	new file:   index.php 	new file:   js/customizer.js 	new file:   js/navigation.js 	new file:   js/skip-link-focus-fix.js 	new file:   languages/readme.txt 	new file:   languages/voyage.pot 	new file:   page.php 	new file:   phpcs.xml.dist 	new file:   readme.txt 	new file:   rtl.css 	new file:   screenshot.png 	new file:   search.php 	new file:   sidebar.php 	new file:   single.php 	new file:   style.css 	new file:   temp.php 	new file:   template-parts/content-none.php 	new file:   template-parts/content-page.php 	new file:   template-parts/content-search.php 	new file:   template-parts/content.php

---
+++ footer.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Voyage
+ */
+?>
+
+<footer>
+	<div class ="container">
+		<div>
+			<p>Copyright &copy;EmanGeoTours <?php echo date('Y')?></p>
+		</div>
+	</div>
+</footer>	
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * The header for our theme
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Voyage
+ */
+
+?>
+<!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="https://gmpg.org/xfn/11">
+
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+
+
+	<header class="site-header">
+		<div><?php bloginfo( 'name' ); ?></div>
+		<div><?php bloginfo( 'description' ); ?></div>
+
+		<div class="site-branding">
+			<?php
+			the_custom_logo();
+			if ( is_front_page() && is_home() ) :
+				?>
+				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+				<?php
+			else :
+				?>
+				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+				<?php
+			endif;
+			$voyage_description = get_bloginfo( 'description', 'display' );
+			if ( $voyage_description || is_customize_preview() ) :
+				?>
+				<p class="site-description"><?php echo $voyage_description; /* WPCS: xss ok. */ ?></p>
+			<?php endif; ?>
+		</div><!-- .site-branding -->
+
+		<nav id="site-navigation" class="main-navigation">
+			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'voyage' ); ?></button>
+			<?php
+			wp_nav_menu( array(
+				'theme_location' => 'header-menu',
+				'container' => false,
+				'menu_class' => 'nav navbar-nav',
+			) );
+			?>
+		</nav><!-- #site-navigation -->
+	</header><!-- #masthead -->
