ae5cf5352399d03ffc1108c345fc151bbd49d6cb
IV <iv.voloshko@gmail.com>
2020-02-29 09:55:24 +0200

    modified:   assets/css/main.css 	modified:   header.php

--- header.php
+++ header.php
@@ -22,10 +22,10 @@
 
 <body <?php body_class(); ?>>
 
-
-	<header class="site-header">
-	
-		<div class="container header-block">
+<section class="banner-block__overlay">
+<!-- <div class="container"> -->
+	<header >	
+		<div class="header-block">
 			<div class="logo">			
 				<div class="logo-img">
 					<?php the_custom_logo( $blog_id ); ?>					
@@ -33,27 +33,26 @@
 				<div class="logo-title">
 					<p>EmanGeoTours<br><span>Cyprus tours</span></p>
 				</div>	
-			</div>	
-			<div>
-			<nav id="site-navigation" class="main-navigation">
-					<!-- <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'voyage' ); ?></button> -->
-					<?php
-					wp_nav_menu( array(
-						'theme_location' => 'header-menu',
-						'container' => false,
-						'menu_class' => 'nav navbar-nav ',
-					) );
-					?>
-				</nav>	
-			</div>						
-		</div>	
-
-		<section class="intro  container">	
-			<div class="banner-block">
+			</div><!--.logo -->			
+			<nav class="main-navigation">
+				<!-- <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'voyage' ); ?></button> -->
+				<?php
+				wp_nav_menu( array(
+					'theme_location' => 'header-menu',
+					'container' => false,
+					'menu_class' => 'nav navbar-nav ',
+				) );
+				?>
+			</nav>										
+		</div><!--.header-block-->
+</header>
+<!-- ******************BANNER***************** -->	
+		<div class="slick-slider intro">	
+			<div class="banner-block slider-bg-1">
 				<div>
 					<h1 class="banner-block__title"><?php bloginfo( 'name' ); ?></h1>
 				</div>			
-				<div class="banner-block__overlay">						
+				<div>						
 					<div  class="banner-block__description">
 						<div class="social-block">
 							<ul class="social-block__item">
@@ -61,19 +60,25 @@
 								<li class="social-block__list"><a href="#" class="social-block__link"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
 							</ul>	
 						</div>
-						<div>
+						<div class="slider-content">
 							<h2 class="banner-block__subtitle"><?php bloginfo( 'description' ); ?></h2>
 							<p class="banner-block__text">
 									Окунись в культуру и историю калоритного Кипра, 
 									в сопровождении опытного гида.
 							</p>
 						</div>						
-					</div>											
+					</div><!-- .banner-block__description-->
 				</div>				
 				<div class="btn">
 					<a class="btn__link" href="#">Выбрать экскурсию</a>
 				</div>			
-				</div>											
-		</section>
+			</div><!--.banner-block-->
+		</div><!--.slick-slider-->											
+	<!-- </div>.container -->
+</section>
 
-	</header>
+	
+	
+	
+
+	
